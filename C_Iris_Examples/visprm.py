351b2d63c05dfdb63b694191b8bc663774cd74f6
Peter <wernerpe@mit.edu>
2022-11-27 14:58:21 -0500

    random seeding baseline

--- C_Iris_Examples/visprm.py
+++ C_Iris_Examples/visprm.py
@@ -254,6 +254,7 @@ class SetVPRM:
 
 import networkx as nx
 import matplotlib.pyplot as plt
+from time import gmtime, strftime
 from pydrake.geometry.optimization import HPolyhedron
 
 def default_sample_ranking(vs):
@@ -279,7 +280,7 @@ def default_sample_ranking(vs):
         if vis_components > max_vis_components:
                 max_vis_components = vis_components
                 key_max = key
-    print('[VPRMSeeding] Num connected Components Vis:', max_vis_components)
+    print(strftime("[%H:%M:%S] ", gmtime()) + '[VPRMSeeding] Num connected Components Vis:', max_vis_components)
 
     return key_max, not (len(samples_outside_regions.keys()) > 0)
 
@@ -310,9 +311,9 @@ class VPRMSeeding:
         self.eps = eps
         self.M = int(np.log(alpha)/np.log(1-eps))
         if self.verbose: 
-            print('[VPRMSeeding] Expecting points of interest in q')
-            print('[VPRMSeeding] GuardInsertion attempts M:', str(self.M))
-            print('[VPRMSeeding] {} probability that unseen region is less than {} "%" of Cfree '.format(1-self.alpha, 100*eps))
+            print(strftime("[%H:%M:%S] ", gmtime()) +'[VPRMSeeding] Expecting points of interest in q')
+            print(strftime("[%H:%M:%S] ", gmtime()) +'[VPRMSeeding] GuardInsertion attempts M:', str(self.M))
+            print(strftime("[%H:%M:%S] ", gmtime()) +'[VPRMSeeding] {} probability that unseen region is less than {} "%" of Cfree '.format(1-self.alpha, 100*eps))
 
 
         self.col_handle = collision_handle
@@ -336,7 +337,7 @@ class VPRMSeeding:
                 self.seed_points = [s for s in self.samples_to_connect]
                 for idx in range(len(self.regions)): self.guard_regions.append(idx) 
             else:
-                raise ValueError("[VPRMSeeding] guard_regions must be an empty list")
+                raise ValueError(strftime("[%H:%M:%S] ", gmtime()) +"[VPRMSeeding] guard_regions must be an empty list")
         else:
             for idx, r in enumerate(regions):
                 seed, reg = r
@@ -369,7 +370,7 @@ class VPRMSeeding:
             good_sample = (not col) and (not self.point_in_guard_regions(pos_samp))
             it+=1
         if not good_sample:
-            raise ValueError("[VPRMSeeding] ERROR: Could not find collision free point in MAXIT %d".format(MAXIT))
+            raise ValueError(strftime("[%H:%M:%S] ", gmtime()) +"[VPRMSeeding] ERROR: Could not find collision free point in MAXIT %d".format(MAXIT))
         return pos_samp
 
     def load_checkpoint(self, checkpoint):
@@ -393,7 +394,7 @@ class VPRMSeeding:
                 r2 = self.regions[idx2]
                 if r1.IntersectsWith(r2):
                     self.connectivity_graph.add_edge(idx1,idx2)
-        print("[VPRMSeeding] Checkpoint loaded successfully, current state is at end of guard phase")
+        print(strftime("[%H:%M:%S] ", gmtime()) +"[VPRMSeeding] Checkpoint loaded successfully, current state is at end of guard phase")
 
     def run(self):
         self.set_guard_regions()
@@ -429,8 +430,7 @@ class VPRMSeeding:
             if add_to_sample_set:
                 self.samples_outside_regions[str(p)] = [p, visible_regions]
             else:
-                print(it)
-                if self.verbose: print("[VPRMSeeding] New guard placed N = ", str(len(self.guard_regions))) 
+                if self.verbose: print(strftime("[%H:%M:%S] ", gmtime()) +"[VPRMSeeding] New guard placed N = ", str(len(self.guard_regions)), "it = ", it) 
                 try:
                     rnew = self.grow_region_at(p)
                     self.regions.append(rnew)
@@ -449,9 +449,9 @@ class VPRMSeeding:
                             self.samples_outside_regions[s_key][1].append(rnew)
                     for k in keys_to_del:
                         del self.samples_outside_regions[k]
-                    if self.verbose: print('[VPRMSeeding] Sample set size',len(self.samples_outside_regions.keys()))
+                    if self.verbose: print(strftime("[%H:%M:%S] ", gmtime()) +'[VPRMSeeding] Sample set size',len(self.samples_outside_regions.keys()))
                 except:
-                    print('[VPRMSeeding] Mosek failed, deleting point')
+                    print(strftime("[%H:%M:%S] ", gmtime()) +'[VPRMSeeding] Mosek failed, deleting point')
             it+=1
 
         self.connectivity_graph = nx.Graph()
@@ -475,7 +475,7 @@ class VPRMSeeding:
             loc_best_sample = self.samples_outside_regions[best_sample][0]
             try:
                 nr = self.grow_region_at_with_obstacles(loc_best_sample.reshape(-1,1), self.regions)
-                if self.verbose: print('[VPRMSeeding] New region added', loc_best_sample.reshape(-1))
+                if self.verbose: print(strftime("[%H:%M:%S] ", gmtime()) +'[VPRMSeeding] New region added', loc_best_sample.reshape(-1))
                 self.regions.append(nr)
                 self.seed_points.append(loc_best_sample.copy())
                 idx_new_region = len(self.regions)-1
@@ -494,13 +494,180 @@ class VPRMSeeding:
                     keys_to_del.append(best_sample)
                 for k in keys_to_del:
                     del self.samples_outside_regions[k]
-                if self.verbose: print('[VPRMSeeding] Sample set size',len(self.samples_outside_regions.keys()), 'num keys to del ', len(keys_to_del))
+                if self.verbose: print(strftime("[%H:%M:%S] ", gmtime()) +'[VPRMSeeding] Sample set size',len(self.samples_outside_regions.keys()), 'num keys to del ', len(keys_to_del))
                 #update connectivity graph
                 for idx, r in enumerate(self.regions[:-1]):
                     if r.IntersectsWith(nr):
                         self.connectivity_graph.add_edge(idx, idx_new_region)
             except:
-                print('[VPRMSeeding] Failed, deleting point')
+                print(strftime("[%H:%M:%S] ", gmtime()) +'[VPRMSeeding] Failed, deleting point')
                 del self.samples_outside_regions[best_sample]
         return done_connecting
 
+
+class RandSeeding:
+    def __init__(self,
+                 samples_to_connect,
+                 limits = None,
+                 alpha = 0.05,
+                 eps = 0.05,
+                 collision_handle = None,
+                 iris_handle = None,
+                 iris_handle_with_obstacles = None,
+                 point_to_region_conversion = None,
+                 plot_node = None,
+                 plot_edge = None,
+                 plot_region = None,
+                 Verbose = True):
+
+        self.verbose = Verbose
+        self.limits = limits
+        self.min_pos = self.limits[0]
+        self.max_pos = self.limits[1]
+        self.min_max_diff = self.max_pos - self.min_pos 
+        self.dim = len(self.min_pos)
+        self.alpha = alpha
+        self.eps = eps
+        self.M = int(np.log(alpha)/np.log(1-eps))
+        if self.verbose: 
+            print(strftime("[%H:%M:%S] ", gmtime()) +'[RandSeeding] Expecting points of interest in q')
+            print(strftime("[%H:%M:%S] ", gmtime()) +'[RandSeeding] GuardInsertion attempts M:', str(self.M))
+            print(strftime("[%H:%M:%S] ", gmtime()) +'[RandSeeding] {} probability that unseen region is less than {} "%" of Cfree '.format(1-self.alpha, 100*eps))
+
+
+        self.col_handle = collision_handle
+        self.grow_region_at = iris_handle
+        self.grow_region_at_with_obstacles = iris_handle_with_obstacles 
+        self.regions = []
+        self.seed_points = []
+        self.samples_to_connect = samples_to_connect
+        self.nodes_to_connect = set([idx for idx, s in enumerate(self.samples_to_connect)])
+        self.point_to_region_space = point_to_region_conversion
+        self.need_to_convert_samples = True if self.point_to_region_space is not None else False
+
+    def set_init_regions(self, regions = None):
+        if regions is None:
+            if len(self.regions)==0:
+                self.regions = [self.grow_region_at(r) for r in self.samples_to_connect]
+                self.seed_points = [s for s in self.samples_to_connect]
+            else:
+                raise ValueError(strftime("[%H:%M:%S] ", gmtime()) +"[RandSeeding] regions must be an empty list")
+        else:
+            for idx, r in enumerate(regions):
+                seed, reg = r
+                self.regions.append(reg)
+                self.seed_points.append(seed)
+                
+    def point_in_regions(self, q):
+        if self.need_to_convert_samples:
+            pt = self.point_to_region_space(q)
+        else:
+            pt = q
+        for r in self.regions:
+            if r.PointInSet(pt):
+                    return True
+        return False
+
+    def sample_node_pos(self, MAXIT = 1e4):  
+        rand = np.random.rand(self.dim)
+        pos_samp = self.min_pos + rand*self.min_max_diff 
+        good_sample = (not self.col_handle(pos_samp)) 
+        it = 0
+        while not good_sample and it < MAXIT:
+            rand = np.random.rand(self.dim)
+            pos_samp = self.min_pos + rand*self.min_max_diff 
+            col = False
+            for _ in range(10):
+                r  = 0.01*(np.random.rand(self.dim)-0.5)
+                col |= (self.col_handle(pos_samp+r) > 0)
+            good_sample = not col
+            it+=1
+        if not good_sample:
+            raise ValueError(strftime("[%H:%M:%S] ", gmtime()) +"[RandSeeding] ERROR: Could not find collision free point in MAXIT %d".format(MAXIT))
+        return pos_samp
+
+    def load_checkpoint(self, checkpoint):
+        self.seed_points = checkpoint['seedpoints']
+        A = checkpoint['regionsA']
+        B = checkpoint['regionsB']
+        self.regions = [HPolyhedron(a,b) for a,b, in zip(A,B)]
+        #vis_reg = [[self.regions[idx] for idx in vis] for vis in checkpoint['sample_set_vis_regions']]
+        #for i ,pt in enumerate(checkpoint['sample_set_points']):
+        #    self.samples_outside_regions[str(pt)] = [pt, vis_reg[i]]
+        
+        self.connectivity_graph = nx.Graph()
+        for idx in range(len(self.regions)):
+            self.connectivity_graph.add_node(idx)
+            
+        for idx1 in range(len(self.regions)):
+            for idx2 in range(idx1 +1, len(self.regions)):
+                r1 = self.regions[idx1]
+                r2 = self.regions[idx2]
+                if r1.IntersectsWith(r2):
+                    self.connectivity_graph.add_edge(idx1,idx2)
+        print(strftime("[%H:%M:%S] ", gmtime()) +"[RandSeeding] Checkpoint loaded successfully, current state is at end of guard phase")
+
+    def run(self):
+        self.set_init_regions()
+        self.sample_regions_phase()        
+        done_connecting = self.connectivity_phase()
+            
+    def draw_connectivity_graph(self,):
+        fig = plt.figure()
+        colors = []
+        for idx in range(len(self.regions)):
+            if idx<len(self.samples_to_connect):
+                colors.append('c')
+            elif idx<len(self.regions):
+                colors.append('b')
+            else:
+                colors.append('m')
+        nx.draw_spring(self.connectivity_graph, 
+                              with_labels = True, 
+                              node_color = colors)
+
+    def sample_regions_phase(self,):
+        it = 0
+        self.connectivity_graph = nx.Graph()
+        for idx in range(len(self.regions)):
+            self.connectivity_graph.add_node(idx)
+
+        for idx1 in range(len(self.regions)):
+            for idx2 in range(idx1 +1, len(self.regions)):
+                r1 = self.regions[idx1]
+                r2 = self.regions[idx2]
+                if r1.IntersectsWith(r2):
+                    self.connectivity_graph.add_edge(idx1,idx2)
+
+        while it < self.M:
+            p = self.sample_node_pos()
+            if self.point_in_regions(p):
+                it+=1
+            else:
+                if self.verbose: print(strftime("[%H:%M:%S] ", gmtime()) +"[RandSeeding] New Region placed N = ", str(len(self.regions)), ", it = ", str(it)) 
+                try:
+                    rnew = self.grow_region_at_with_obstacles(p.reshape(-1, 1), self.regions)
+                    self.regions.append(rnew)
+                    it = 0
+                    idx_new_region = len(self.regions)-1
+                    self.connectivity_graph.add_node(idx_new_region)
+                    #update connectivity graph
+                    for idx, r in enumerate(self.regions[:-1]):
+                        if r.IntersectsWith(rnew):
+                            self.connectivity_graph.add_edge(idx, idx_new_region)
+                    #check if all points in one connected component
+                    #get connected components
+                    components = [list(a) for a in nx.connected_components(self.connectivity_graph)]
+                    #check if all nodes to connect are part of a single connected component
+                    for c in components:
+                        if self.nodes_to_connect & set(c) == self.nodes_to_connect:
+                            return True
+                except:
+                    print(strftime("[%H:%M:%S] ", gmtime()) +'[RandSeeding] Mosek failed, deleting point')
+        return False    
+        
+            
+        
+        #if self.verbose: print("[VPRMSeeding] Connectivity phase")
+
+   
