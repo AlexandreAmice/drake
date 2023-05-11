f552c95f9c31db8a0f5fd0cfee86d9ee3d87c41e
amice <amice@mit.edu>
2023-05-10 23:44:12 -0400

    plane tests work

--- geometry/optimization/dev/test/cspace_free_path_separating_plane_test.cc
+++ geometry/optimization/dev/test/cspace_free_path_separating_plane_test.cc
@@ -1,13 +1,14 @@
+#include "drake/geometry/optimization/dev/cspace_free_path_separating_plane.h"
+
 #include <gtest/gtest.h>
 
 #include "drake/common/test_utilities/symbolic_test_util.h"
-#include "drake/geometry/optimization/dev/cspace_free_path_separating_plane.h"
 
 namespace drake {
 namespace geometry {
 namespace optimization {
 
-GTEST_TEST(CalcPlane, TestAllSymbolic) {
+GTEST_TEST(CalcPathPlane, TestAllSymbolic) {
   symbolic::Variable mu("mu");
   for (int plane_degree = 1; plane_degree < 3; ++plane_degree) {
     const int num_coeffs_per_poly = plane_degree + 1;
@@ -24,64 +25,94 @@ GTEST_TEST(CalcPlane, TestAllSymbolic) {
     int decision_var_ctr = 0;
     for (int i = 0; i < 3; ++i) {
       symbolic::Polynomial::MapType expected_poly_map;
-      for(int j = 0; j < num_coeffs_per_poly; ++j) {
-        expected_poly_map.insert({symbolic::Monomial{mu, plane_degree-j},
-                                 decision_vars(decision_var_ctr)});
+      for (int j = 0; j < num_coeffs_per_poly; ++j) {
+        expected_poly_map.insert({symbolic::Monomial{mu, plane_degree - j},
+                                  decision_vars(decision_var_ctr)});
         ++decision_var_ctr;
       }
-      EXPECT_PRED2(
-          symbolic::test::PolyEqual, a(i),
-          symbolic::Polynomial(expected_poly_map));
+      EXPECT_PRED2(symbolic::test::PolyEqual, a(i),
+                   symbolic::Polynomial(expected_poly_map));
     }
     symbolic::Polynomial::MapType expected_poly_map;
-      for(int j = 0; j < num_coeffs_per_poly; ++j) {
-        expected_poly_map.insert({symbolic::Monomial{mu, plane_degree-j},
-                                 decision_vars(decision_var_ctr)});
+    for (int j = 0; j < num_coeffs_per_poly; ++j) {
+      expected_poly_map.insert({symbolic::Monomial{mu, plane_degree - j},
+                                decision_vars(decision_var_ctr)});
+      ++decision_var_ctr;
+    }
+    EXPECT_PRED2(symbolic::test::PolyEqual, b,
+                 symbolic::Polynomial(expected_poly_map));
+  }
+}
+
+//// Test decision_vars taking double values and mu takes symbolic values.
+GTEST_TEST(CalcPathPlane, TestDoubleDecisionVariableSymbolicMu) {
+  symbolic::Variable mu("mu");
+  for (int plane_degree = 1; plane_degree < 3; ++plane_degree) {
+    const int num_coeffs_per_poly = plane_degree + 1;
+    const int num_decision_vars = 4 * num_coeffs_per_poly;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> decision_vars{num_decision_vars};
+    for (int i = 0; i < num_decision_vars; ++i) {
+      decision_vars(i) = i + 2;
+    }
+    Vector3<symbolic::Polynomial> a;
+    symbolic::Polynomial b;
+    CalcPathPlane<double, symbolic::Variable, symbolic::Polynomial>(
+        decision_vars, mu, plane_degree, &a, &b);
+    int decision_var_ctr = 0;
+    for (int i = 0; i < 3; ++i) {
+      symbolic::Polynomial::MapType expected_poly_map;
+      for (int j = 0; j < num_coeffs_per_poly; ++j) {
+        expected_poly_map.insert({symbolic::Monomial{mu, plane_degree - j},
+                                  decision_vars(decision_var_ctr)});
         ++decision_var_ctr;
       }
+      EXPECT_PRED2(symbolic::test::PolyEqual, a(i),
+                   symbolic::Polynomial(expected_poly_map));
+    }
+    symbolic::Polynomial::MapType expected_poly_map;
+    for (int j = 0; j < num_coeffs_per_poly; ++j) {
+      expected_poly_map.insert({symbolic::Monomial{mu, plane_degree - j},
+                                decision_vars(decision_var_ctr)});
+      ++decision_var_ctr;
+    }
     EXPECT_PRED2(symbolic::test::PolyEqual, b,
                  symbolic::Polynomial(expected_poly_map));
   }
 }
 //
-//// Test decision_vars taking double values and s takes symbolic values.
-//GTEST_TEST(CalcPlane, TestDoubleDecisionVariableSymbolicS) {
-//  symbolic::Variable s("s");
-//  Eigen::Matrix<double, 8, 1> decision_var_vals;
-//  for (int i = 0; i < 8; ++i) {
-//    decision_var_vals(i) = i + 1;
-//  }
-//  Vector3<symbolic::Polynomial> a;
-//  symbolic::Polynomial b;
-//  CalcPlane<double, symbolic::Variable, symbolic::Polynomial>(
-//      decision_var_vals, Vector1<symbolic::Variable>(s),
-//      SeparatingPlaneOrder::kAffine, &a, &b);
-//  for (int i = 0; i < 3; ++i) {
-//    EXPECT_PRED2(symbolic::test::PolyEqual, a(i),
-//                 symbolic::Polynomial(decision_var_vals(i) * s +
-//                                      decision_var_vals(3 + i)));
-//  }
-//  EXPECT_PRED2(
-//      symbolic::test::PolyEqual, b,
-//      symbolic::Polynomial(decision_var_vals(6) * s + decision_var_vals(7)));
-//}
-//
-//// Test with both decision variable and s taking double values.
-//GTEST_TEST(CalcPlane, TestDoubleDecisionVariableDoubleS) {
-//  const double s = 2;
-//  Eigen::Matrix<double, 8, 1> decision_var_vals;
-//  for (int i = 0; i < 8; ++i) {
-//    decision_var_vals(i) = i + 1;
-//  }
-//  Eigen::Vector3d a;
-//  double b;
-//  CalcPlane<double, double, double>(decision_var_vals, Vector1d(s),
-//                                    SeparatingPlaneOrder::kAffine, &a, &b);
-//  for (int i = 0; i < 3; ++i) {
-//    EXPECT_EQ(a(i), decision_var_vals(i) * s + decision_var_vals(3 + i));
-//  }
-//  EXPECT_EQ(b, decision_var_vals(6) * s + decision_var_vals(7));
-//}
+//// Test with both decision variable and mu taking double values.
+GTEST_TEST(CalcPlane, TestDoubleDecisionVariableDoubleMu) {
+  const double mu = 3;
+  for (int plane_degree = 1; plane_degree < 3; ++plane_degree) {
+    const int num_coeffs_per_poly = plane_degree + 1;
+    const int num_decision_vars = 4 * num_coeffs_per_poly;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> decision_vars{num_decision_vars};
+    for (int i = 0; i < num_decision_vars; ++i) {
+      decision_vars(i) = i + 2;
+    }
+    Eigen::Vector3d a = Eigen::Vector3d::Zero();
+    double b{0};
+    CalcPathPlane<double, double, double>(decision_vars, mu, plane_degree, &a,
+                                          &b);
+    int decision_var_ctr = 0;
+    for (int i = 0; i < 3; ++i) {
+      double expected_ret{0};
+      for (int j = 0; j < num_coeffs_per_poly; ++j) {
+        expected_ret +=
+            std::pow(mu, plane_degree - j) * decision_vars(decision_var_ctr);
+        ++decision_var_ctr;
+      }
+      EXPECT_EQ(a(i), expected_ret);
+    }
+    double expected_ret{0};
+    for (int j = 0; j < num_coeffs_per_poly; ++j) {
+      expected_ret +=
+          std::pow(mu, plane_degree - j) * decision_vars(decision_var_ctr);
+      ++decision_var_ctr;
+    }
+    EXPECT_EQ(b, expected_ret);
+  }
+}
 
 }  // namespace optimization
 }  // namespace geometry
