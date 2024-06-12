0465aa77f336f430827f6d456c8243b1155ad544
amice <amice@mit.edu>
2024-06-12 16:30:15 -0400

    ConicStandardFormObject

--- solvers/conic_standard_form.cc
+++ solvers/conic_standard_form.cc
@@ -35,19 +35,16 @@ void CheckSupported(const MathematicalProgram& prog) {
                                       supported_attributes,
                                       &unsupported_message)) {
     throw std::runtime_error(fmt::format(
-        "ParseToConicStandardForm() does not (yet) support this program: {}.",
+        "ConicStandardForm() does not (yet) support this program: {}.",
         unsupported_message));
   }
 }
 
 }  // namespace
 
-void ParseToConicStandardForm(
-    const MathematicalProgram& prog, Eigen::SparseVector<double>* c, double* d,
-    Eigen::SparseMatrix<double>* A, Eigen::SparseVector<double>* b,
-    std::unordered_map<ProgramAttribute, std::vector<std::pair<int, int>>>*
-        attributes_to_start_end_pairs) {
+ConicStandardForm::ConicStandardForm(const MathematicalProgram& prog) {
   CheckSupported(prog);
+
   internal::ConvexConstraintAggregationInfo info;
   internal::ConvexConstraintAggregationOptions options;
   options.cast_rotated_lorentz_to_lorentz = true;
@@ -55,12 +52,11 @@ void ParseToConicStandardForm(
   options.parse_psd_using_upper_triangular = false;
 
   std::vector<double> c_std(prog.num_vars(), 0.0);
-  (*d) = 0;
-  internal::ParseLinearCosts(prog, &c_std, d);
-  c->resize(c_std.size());
+  internal::ParseLinearCosts(prog, &c_std, &d_);
+  c_.resize(c_std.size());
   for (int i = 0; i < ssize(c_std); ++i) {
     if (c_std[i] != 0.0) {
-      c->insert(i) = c_std[i];
+      c_.insert(i) = c_std[i];
     }
   }
 
@@ -71,23 +67,23 @@ void ParseToConicStandardForm(
                                                 info.A_triplets[i].col(),
                                                 -info.A_triplets[i].value());
   }
-  A->resize(info.A_row_count, prog.num_vars());
-  A->setFromTriplets(info.A_triplets.begin(), info.A_triplets.end());
-  b->resize(info.b_std.size());
+  A_.resize(info.A_row_count, prog.num_vars());
+  A_.setFromTriplets(info.A_triplets.begin(), info.A_triplets.end());
+
+  b_.resize(info.b_std.size());
   for (int i = 0; i < ssize(info.b_std); ++i) {
     if (info.b_std[i] != 0.0) {
-      b->insert(i) = info.b_std[i];
+      b_.insert(i) = info.b_std[i];
     }
   }
 
   int expected_A_row_count = 0;
-  attributes_to_start_end_pairs->clear();
-  attributes_to_start_end_pairs->emplace(
+  attributes_to_start_end_pairs_.emplace(
       ProgramAttribute::kLinearEqualityConstraint,
       std::vector<std::pair<int, int>>{});
   if (info.num_linear_equality_constraint_rows > 0) {
-    attributes_to_start_end_pairs
-        ->at(ProgramAttribute::kLinearEqualityConstraint)
+    attributes_to_start_end_pairs_
+        .at(ProgramAttribute::kLinearEqualityConstraint)
         .emplace_back(0, info.num_linear_equality_constraint_rows);
   }
   expected_A_row_count += info.num_linear_equality_constraint_rows;
@@ -96,40 +92,83 @@ void ParseToConicStandardForm(
       info.num_linear_constraint_rows +
       info.num_bounding_box_inequality_constraint_rows;
 
-  attributes_to_start_end_pairs->emplace(ProgramAttribute::kLinearConstraint,
+  attributes_to_start_end_pairs_.emplace(ProgramAttribute::kLinearConstraint,
                                          std::vector<std::pair<int, int>>{});
   if (total_num_linear_constraints > 0) {
-    attributes_to_start_end_pairs->at(ProgramAttribute::kLinearConstraint)
+    attributes_to_start_end_pairs_.at(ProgramAttribute::kLinearConstraint)
         .emplace_back(expected_A_row_count,
                       expected_A_row_count + total_num_linear_constraints);
   }
   expected_A_row_count += total_num_linear_constraints;
 
-  attributes_to_start_end_pairs->emplace(
+  attributes_to_start_end_pairs_.emplace(
       ProgramAttribute::kLorentzConeConstraint,
       std::vector<std::pair<int, int>>{});
-  attributes_to_start_end_pairs->at(ProgramAttribute::kLorentzConeConstraint)
+  attributes_to_start_end_pairs_.at(ProgramAttribute::kLorentzConeConstraint)
       .reserve(info.second_order_cone_lengths.size());
   for (const int soc_length : info.second_order_cone_lengths) {
-    attributes_to_start_end_pairs->at(ProgramAttribute::kLorentzConeConstraint)
+    attributes_to_start_end_pairs_.at(ProgramAttribute::kLorentzConeConstraint)
         .emplace_back(expected_A_row_count, expected_A_row_count + soc_length);
     expected_A_row_count += soc_length;
   }
 
-  attributes_to_start_end_pairs->emplace(
+  attributes_to_start_end_pairs_.emplace(
       ProgramAttribute::kPositiveSemidefiniteConstraint,
       std::vector<std::pair<int, int>>{});
-  attributes_to_start_end_pairs
-      ->at(ProgramAttribute::kPositiveSemidefiniteConstraint)
+  attributes_to_start_end_pairs_
+      .at(ProgramAttribute::kPositiveSemidefiniteConstraint)
       .reserve(info.psd_row_size.size());
   for (const int row_size : info.psd_row_size) {
     int psd_length = row_size * (row_size + 1) / 2;
-    attributes_to_start_end_pairs
-        ->at(ProgramAttribute::kPositiveSemidefiniteConstraint)
+    attributes_to_start_end_pairs_
+        .at(ProgramAttribute::kPositiveSemidefiniteConstraint)
         .emplace_back(expected_A_row_count, expected_A_row_count + psd_length);
     expected_A_row_count += psd_length;
   }
-  DRAKE_DEMAND(expected_A_row_count == A->rows());
+  DRAKE_DEMAND(expected_A_row_count == A_.rows());
+}
+
+std::unique_ptr<MathematicalProgram> ConicStandardForm::MakeProgram() const {
+  std::unique_ptr<MathematicalProgram> prog_standard_form =
+      std::make_unique<MathematicalProgram>();
+  auto x = prog_standard_form->NewContinuousVariables(A_.cols(), "x");
+  prog_standard_form->AddLinearCost(c_.toDense(), d_, x);
+
+  for (const auto& [attribute, index_pairs] : attributes_to_start_end_pairs_) {
+    for (const auto& [start, end] : index_pairs) {
+      const int length = end - start;
+      if (attribute == ProgramAttribute::kLinearEqualityConstraint) {
+        prog_standard_form->AddLinearEqualityConstraint(
+            A_.middleRows(start, length), -b_.segment(start, length).toDense(),
+            x);
+      } else if (attribute == ProgramAttribute::kLinearConstraint) {
+        prog_standard_form->AddLinearConstraint(
+            A_.middleRows(start, length), -b_.segment(start, length).toDense(),
+            Eigen::VectorXd::Constant(length, kInf), x);
+      } else if (attribute == ProgramAttribute::kLorentzConeConstraint) {
+        prog_standard_form->AddLorentzConeConstraint(
+            A_.middleRows(start, length).toDense(),
+            b_.segment(start, length).toDense(), x);
+      } else if (attribute ==
+                 ProgramAttribute::kPositiveSemidefiniteConstraint) {
+        const MatrixX<symbolic::Expression> y_vec =
+            A_.middleRows(start, length).toDense() *
+                x.cast<symbolic::Expression>() +
+            b_.segment(start, length).toDense();
+        MatrixX<symbolic::Expression> Y =
+            math::ToSymmetricMatrixFromLowerTriangularColumns(y_vec);
+        const double sqrt2 = std::sqrt(2);
+        for (int i = 0; i < Y.rows(); ++i) {
+          for (int j = i + 1; j < Y.cols(); ++j) {
+            Y(i, j) = (Y(i, j) / sqrt2).Expand();
+            Y(j, i) = Y(i, j);
+          }
+        }
+        prog_standard_form->AddPositiveSemidefiniteConstraint(Y);
+      }
+    }
+  }
+  return prog_standard_form;
 }
 }  // namespace solvers
 }  // namespace drake
--- solvers/conic_standard_form.h
+++ solvers/conic_standard_form.h
@@ -21,6 +21,61 @@ namespace solvers {
 // std::unique_ptr<MathematicalProgram> ParseToConicStandardForm(
 //    const MathematicalProgram& prog);
 
+/** Stores the information required to represent a convex program  (specficially
+ * an LP, QP, SOCP, or SDP), in standard primal conic
+ * form:
+ * min 〈 c, x 〉+ d subject to
+ * Ax + b ∈ K
+ * Where K is the product of the cones:
+ * 1) Zero cone {x | x = 0 }
+ * 2) Positive orthant {x | x ≥ 0 }
+ * 3) Second-order cone {(t, x) | |x|₂ ≤ t }
+ * 4) Positive semidefinite cone { X |  min(eig(X)) ≥ 0, X = Xᵀ }
+ */
+class ConicStandardForm {
+ public:
+  ConicStandardForm(const MathematicalProgram& prog);
+
+  const Eigen::SparseVector<double>& c() const { return c_; }
+  double d() const { return d_; }
+  const Eigen::SparseMatrix<double>& A() const { return A_; }
+  const Eigen::SparseVector<double>& b() const { return b_; }
+
+  const std::unordered_map<ProgramAttribute, std::vector<std::pair<int, int>>>&
+  attributes_to_start_end_pairs() const {
+    return attributes_to_start_end_pairs_;
+  }
+
+  /** Return a MathematicalProgram represented by this standard conic form data.
+   */
+  std::unique_ptr<MathematicalProgram> MakeProgram() const;
+
+ private:
+  /** c a sparse vector encoding the cost. */
+  Eigen::SparseVector<double> c_;
+  /** d is the constant term in the cost. */
+  double d_{0};
+  /** A is the sparse matrix encoding the conic constraint. */
+  Eigen::SparseMatrix<double> A_;
+  /** b is a sparse matrix encoding the conic constraint. */
+  Eigen::SparseVector<double> b_;
+
+  /**
+   * attributes_to_start_end_pairs[type][i] is a pair of indices
+   * [start, end) encoding that (Ax+b)[start:end, :] is in the cone
+   * corresponding to the attribute type.
+   * Note that if (Ax+b)[start:end, :] is in the psd cone, then it is a vector
+   * corresponding to the lower-triangular part of a PSD matrices with the
+   * off-diagonal entires scaled by √2 to preserve inner products. For example
+   * if (Ax+b)[start:end, :] = [y0, y1, y2, y3, y4, y5], then
+   * Y = [y0,    y1/√2,  y2/√2]
+   *     [y1/√2,   y3,  y4/√2]
+   *     [y2/√2, y4/√2, y5]]
+   * */
+  std::unordered_map<ProgramAttribute, std::vector<std::pair<int, int>>>
+      attributes_to_start_end_pairs_{};
+};
+
 /**
  * Given a convex program (specficially an LP, QP, SOCP, or SDP), parse it into
  * an equivalent program in standard primal conic form:
@@ -32,28 +87,9 @@ namespace solvers {
  * 3) Second-order cone {(t, x) | |x|₂ ≤ t }
  * 4) Positive semidefinite cone { X | min(eig(X)) ≥ 0, X = Xᵀ }
  *
- * [out] c a sparse vector encoding the cost.
- * [out] A is the sparse matrix encoding the constraint.
- * [out] b is the vector encoding the constraint
- * [out] attributes_to_start_end_pairs[type][i] is a pair of indices [start,
- * end) encoding that (Ax+b)[start:end, :] is in the cone corresponding to the
- * attribute type.
- *
- * Note that if (Ax+b)[start:end, :] is in the psd cone, then it is a vector
- * corresponding to the lower-triangular part of a PSD matrices with the
- * off-diagonal entires scaled by √2 to preserve inner products. For example
- * if (Ax+b)[start:end, :] = [y0, y1, y2, y3, y4, y5], then
- * Y = [y0,    y1/√2,  y2/√2]
- *     [y1/√2,   y3,  y4/√2]
- *     [y2/√2, y4/√2, y5]]
- *
- * Currently, I only support programs with linear costs.
+ * Currently, we only support programs with linear costs.
  */
-void ParseToConicStandardForm(
-    const MathematicalProgram& prog, Eigen::SparseVector<double>* c, double* d,
-    Eigen::SparseMatrix<double>* A, Eigen::SparseVector<double>* b,
-    std::unordered_map<ProgramAttribute, std::vector<std::pair<int, int>>>*
-        attributes_to_start_end_pairs);
+ConicStandardForm ParseToConicStandardForm(const MathematicalProgram& prog);
 
 }  // namespace solvers
 }  // namespace drake
