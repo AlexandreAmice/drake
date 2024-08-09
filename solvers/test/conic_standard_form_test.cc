beef72cb8f6c45a282e84ec78ce25c6cb968502b
amice <amice@mit.edu>
2024-08-09 11:58:40 -0400

    add decision variable access

--- solvers/test/conic_standard_form_test.cc
+++ solvers/test/conic_standard_form_test.cc
@@ -45,14 +45,21 @@ void CheckParseToConicStandardForm(const MathematicalProgram& prog) {
         prog.positive_semidefinite_constraints().size() +
                     prog.linear_matrix_inequality_constraints().size() ==
                 0
-            ? 1e-9
-            : 1e-6;
+            ? 1e-8
+            : 1e-4;
     EXPECT_NEAR(original_result.get_optimal_cost(),
                 standard_form_result.get_optimal_cost(), kTol);
     EXPECT_TRUE(CompareMatrices(
         original_result.get_x_val(),
         standard_form_result.get_x_val().head(prog.decision_variables().size()),
         kTol, MatrixCompareType::relative));
+
+    // Check that the decision variables of the original program are used in the
+    // standard form program.
+    EXPECT_TRUE(
+        CompareMatrices(original_result.GetSolution(prog.decision_variables()),
+                        original_result.GetSolution(standard_form.x()), 1e-10,
+                        MatrixCompareType::relative));
   }
 }
 }  // namespace
