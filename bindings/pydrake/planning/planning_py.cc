4da25d997c788cd08a460ef27db3333ca566cfdf
Michaelszeng <michaelszeng@gmail.com>
2024-09-25 11:55:36 -0400

    wip porting over clique inflation

--- bindings/pydrake/planning/planning_py.cc
+++ bindings/pydrake/planning/planning_py.cc
@@ -28,6 +28,7 @@ and/or trajectories of dynamical systems.
   internal::DefinePlanningTrajectoryOptimization(m);
   internal::DefinePlanningVisibilityGraph(m);
   internal::DefinePlanningFastIris(m);
+  internal::DefinePlanningFastCliqueInflation(m);
   internal::DefinePlanningIrisFromCliqueCover(m);
   internal::DefinePlanningIrisZo(m);
   internal::DefinePlanningZmpPlanner(m);
