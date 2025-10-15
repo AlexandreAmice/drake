04fdc3d435bacb9e5ac45f84585b1cf03832fb37
amice <amice@mit.edu>
2025-10-15 17:10:50 -0400

    patch the bindings for master

--- bindings/pydrake/solvers/solvers_py_conic_standard_form.cc
+++ bindings/pydrake/solvers/solvers_py_conic_standard_form.cc
@@ -1,5 +1,5 @@
+#include "drake/bindings/generated_docstrings/solvers.h"
 #include "drake/bindings/pydrake/common/eigen_pybind.h"
-#include "drake/bindings/pydrake/documentation_pybind.h"
 #include "drake/bindings/pydrake/pydrake_pybind.h"
 #include "drake/bindings/pydrake/solvers/solvers_py.h"
 #include "drake/bindings/pydrake/symbolic_types_pybind.h"
@@ -12,12 +12,11 @@ namespace internal {
 void DefineSolversConicStandardForm(py::module m) {
   // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
   using namespace drake::solvers;
-  constexpr auto& doc = pydrake_doc.drake.solvers;
-  const auto& cls_doc = doc.ConicStandardForm;
-  py::class_<ConicStandardForm>(m, "ConicStandardForm", cls_doc.doc)
-      .def(py::init<const MathematicalProgram&>(), py::arg("prog"),
-          cls_doc.ctor.doc)
-      .def("A", &ConicStandardForm::A, cls_doc.A.doc)
+  // constexpr auto& doc = pydrake_doc_solvers.drake.solvers;
+  // const auto& cls_doc = doc.ConicStandardForm;
+  py::class_<ConicStandardForm>(m, "ConicStandardForm")
+      .def(py::init<const MathematicalProgram&>(), py::arg("prog"))
+      .def("A", &ConicStandardForm::A)
       // TODO(Alexandre.Amice) when pybind 11 supports sparse vectors simplify
       // this binding.
       .def(
@@ -25,30 +24,26 @@ void DefineSolversConicStandardForm(py::module m) {
           [](const ConicStandardForm& self) {
             Eigen::SparseMatrix<double> b(self.b().rows(), 1);
             for (Eigen::SparseVector<double>::InnerIterator it(self.b()); it;
-                ++it) {
+                 ++it) {
               b.insert(it.index(), 0) = it.value();
             }
             return b;
-          },
-          cls_doc.b.doc)
+          })
       .def(
           "c",
           [](const ConicStandardForm& self) {
             Eigen::SparseMatrix<double> c(self.c().rows(), 1);
             for (Eigen::SparseVector<double>::InnerIterator it(self.c()); it;
-                ++it) {
+                 ++it) {
               c.insert(it.index(), 0) = it.value();
             }
             return c;
-          },
-          cls_doc.c.doc)
-      .def("d", &ConicStandardForm::d, cls_doc.d.doc)
-      .def("x", &ConicStandardForm::x, cls_doc.x.doc)
+          })
+      .def("d", &ConicStandardForm::d)
+      .def("x", &ConicStandardForm::x)
       .def("attributes_to_start_end_pairs",
-          &ConicStandardForm::attributes_to_start_end_pairs,
-          cls_doc.attributes_to_start_end_pairs.doc)
-      .def("MakeProgram", &ConicStandardForm::MakeProgram,
-          cls_doc.MakeProgram.doc);
+          &ConicStandardForm::attributes_to_start_end_pairs)
+      .def("MakeProgram", &ConicStandardForm::MakeProgram);
 }
 
 }  // namespace internal
